033ae261c1780b2717ce94078245d9d15e948c65
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-14 13:49:08 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -549,11 +549,21 @@ export default class Endless {
    * @desc
    * Iterator function for an Endless instance
    * Use at your own risk.
+   * @example
+   * const c = new Endless(x => x);
+   * let i = 0;
+   * for(let x of c){
+   *  console.log(x);
+   *  if(i++ > 10) break;
+   * }
    */
   [Symbol.iterator]() {
     let i = 0;
     return {
-      next: () => this[i++],
+      next: () => ({
+        value: this[i++],
+        done: false,
+      }),
     };
   }
 }
